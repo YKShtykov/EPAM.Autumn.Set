04f77f2f53fc9acdbb29af41bc7f42db2a25461a
YKShtykov <yakov.shtykov@gmail.com>
2016-10-16 16:45:24 +0300

    initial commit

---
+++ Set/BL/Set.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    /// <summary>
+    /// Generic collection-set, elements must implement IEquatable interface
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Set<T> where T: IEquatable<T>
+    {
+        private T[] store;
+        private int capacity;
+        private int currentPosition;
+
+        public Set()
+        {
+            capacity = 10;
+            currentPosition = 0;
+            store = new T[capacity];
+        }
+
+        private Set(T[] otherStore)
+        {
+            store = (T[])otherStore.Clone();
+            capacity = otherStore.Length;
+            currentPosition = otherStore.Length;
+        }
+
+        /// <summary>
+        /// Intersection between two collections
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns>new set-intersection</returns>
+        public static Set<T> Intersection(Set<T> lhs, Set<T> rhs)
+        {
+            return new Set<T>(lhs.GetElements().Intersect(rhs.GetElements()).ToArray());
+        }
+
+        /// <summary>
+        /// Union of two collections
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns>new set-union</returns>
+        public static Set<T> Union(Set<T> lhs, Set<T> rhs)
+        {
+            return new Set<T>(lhs.GetElements().Union(rhs.GetElements()).ToArray());
+        }
+
+        /// <summary>
+        /// Expect the second set from the first
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns>new set expection</returns>
+        public static Set<T> Expect(Set<T> lhs, Set<T> rhs)
+        {
+            return new Set<T>(lhs.GetElements().Except(rhs.GetElements()).ToArray());
+        }
+
+        /// <summary>
+        /// The method removes equals elements form collections and creates new set
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns>new set with elements from two sets</returns>
+        public static Set<T> SymmetricExpect(Set<T> lhs, Set<T> rhs)
+        {
+            var intersection = lhs.GetElements().Intersect(rhs.GetElements());
+            var union = lhs.GetElements().Union(rhs.GetElements());
+            return new Set<T>(union.Except(intersection).ToArray());
+        }
+
+        /// <summary>
+        /// Add element to set
+        /// </summary>
+        /// <param name="element"></param>
+        public void Add(T element)
+        {
+            if (Contains(element)) return;
+
+            try
+            {
+                store[currentPosition] = element;
+                currentPosition++;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                IncreaseCapacity();
+                Add(element);
+            }
+        }
+
+        /// <summary>
+        /// Add a
+        /// </summary>
+        /// <param name="array"></param>
+        public void Add(IEnumerable<T> array)
+        {
+            foreach (var item in array)
+            {
+                Add(item);
+            }
+        }
+
+        public void Remove(T element)
+        {
+            for (int i = 0; i < currentPosition; i++)
+            {
+                if (element.Equals(store[i]))
+                {
+                    RemoveElement(i);
+                    return;
+                }
+            }
+        }
+
+        private bool Contains(T element)
+        {
+            for (int i = 0; i < currentPosition; i++)
+            {
+                if (element.Equals(store[i])) return true;
+            }
+
+            return false;
+        }
+        
+        private void RemoveElement(int elementposition)
+        {
+            for (int i = elementposition; i < currentPosition; i++)
+            {
+                store[i] = store[i + 1];
+            }
+            currentPosition--;
+        }        
+
+        private void IncreaseCapacity()
+        {
+            capacity = capacity + 100;
+            Array.Resize(ref store, capacity);
+        }       
+
+        public IEnumerable<T> GetElements()
+        {
+            var tmp = (T[])store.Clone();
+            Array.Resize(ref tmp, currentPosition);
+            return tmp.AsEnumerable();
+        }
+    }
+}
---
+++ Set/SetNUnitTests/SetTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BL;
+
+namespace SetNUnitTests
+{
+    [TestFixture]
+    public class SetTest
+    {
+        [Test]
+        public void IntersectionTest()
+        {
+            Set<int> a = new Set<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                a.Add(i);
+            }
+            Set<int> b = new Set<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                b.Add(i);
+            }
+
+            Set<int> c = Set<int>.Intersection(a, b);
+
+            CollectionAssert.AreEqual(c.GetElements(),new int[] { 0, 1, 2, 3, 4 });
+        }
+
+        [Test]
+        public void UnionTest()
+        {
+            Set<int> a = new Set<int>();
+            a.Add(new int[]{ 1,2});
+            Set<int> b = new Set<int>();
+            b.Add(new int[]{ 3,5});
+            Set<int> c = Set<int>.Union(a, b);
+
+            CollectionAssert.AreEqual(c.GetElements(), new int[] { 1,2,3,5 });
+        }
+
+        [Test]
+        public void ExpectTest()
+        {
+            Set<int> a = new Set<int>();
+            a.Add(new int[] { 1, 2,3 });
+            Set<int> b = new Set<int>();
+            b.Add(new int[] { 3, 5 });
+            Set<int> c = Set<int>.Expect(a, b);
+
+            CollectionAssert.AreEqual(c.GetElements(), new int[] { 1, 2 });
+        }
+
+        [Test]
+        public void SymmetricExpectTest()
+        {
+            Set<int> a = new Set<int>();
+            a.Add(new int[] { 1, 2, 3 });
+            Set<int> b = new Set<int>();
+            b.Add(new int[] { 3, 5 });
+            Set<int> c = Set<int>.SymmetricExpect(a, b);
+
+            CollectionAssert.AreEqual(c.GetElements(), new int[] { 1, 2,5 });
+        }
+    }
+}
